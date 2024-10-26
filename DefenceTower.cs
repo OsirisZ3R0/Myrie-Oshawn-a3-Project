9683988a0ff3b08d3b216474cf673e211e2658ed
Oshawn Myrie <teeshawnprime@LIVE.CA>
2024-10-25 20:23:55 -0400

    Added Defence tower Class

---
+++ DefenceTower.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interactive_game
+{
+    public class DefenceTower
+    {
+        public int health = 100;
+        public int damage = 20;
+        public float bulletSpeed = 10;
+        public int size = 20;
+        public int timesUpgrade = 0;
+        private int maxUpgrade = 10;
+
+        public DefenceTower() 
+        {
+            
+        }
+
+        public void Shoot()
+        {
+
+        }
+
+    }
+}
